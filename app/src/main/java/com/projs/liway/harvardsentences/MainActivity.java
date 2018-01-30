8c25263dc9b28508f44ed7bab4d78d1485448c85
Dawn Tan <dawntan08@gmail.com>
2018-01-30 22:26:39 +1100

    + randomNum generator in Utils + int array containing Raw file resources + update TextView texts when button is clicked

--- app/src/main/java/com/projs/liway/harvardsentences/MainActivity.java
+++ app/src/main/java/com/projs/liway/harvardsentences/MainActivity.java
@@ -2,7 +2,9 @@ package com.projs.liway.harvardsentences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -10,13 +12,39 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int NUMBER_OF_LISTS = 72;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.list_textView);
-        textView.setText(Utils.readFileFromRaw(this));
+        final TextView textView = (TextView) findViewById(R.id.list_textView);
+        Button button = (Button) findViewById(R.id.random_button);
+        final int[] resources = {R.raw.hs_01, R.raw.hs_02, R.raw.hs_03, R.raw.hs_04, R.raw.hs_05,
+                R.raw.hs_06, R.raw.hs_07, R.raw.hs_08, R.raw.hs_09, R.raw.hs_10, R.raw.hs_11,
+                R.raw.hs_12, R.raw.hs_13, R.raw.hs_14, R.raw.hs_15, R.raw.hs_16, R.raw.hs_17,
+                R.raw.hs_18, R.raw.hs_19, R.raw.hs_20, R.raw.hs_21, R.raw.hs_22, R.raw.hs_23,
+                R.raw.hs_24, R.raw.hs_25, R.raw.hs_26, R.raw.hs_27, R.raw.hs_28, R.raw.hs_29,
+                R.raw.hs_30, R.raw.hs_31, R.raw.hs_32, R.raw.hs_33, R.raw.hs_34, R.raw.hs_35,
+                R.raw.hs_36, R.raw.hs_37, R.raw.hs_38, R.raw.hs_39, R.raw.hs_40, R.raw.hs_41,
+                R.raw.hs_42, R.raw.hs_43, R.raw.hs_44, R.raw.hs_45, R.raw.hs_46, R.raw.hs_47,
+                R.raw.hs_48, R.raw.hs_49, R.raw.hs_50, R.raw.hs_51, R.raw.hs_52, R.raw.hs_53,
+                R.raw.hs_54, R.raw.hs_55, R.raw.hs_56, R.raw.hs_57, R.raw.hs_58, R.raw.hs_59,
+                R.raw.hs_60, R.raw.hs_61, R.raw.hs_62, R.raw.hs_63, R.raw.hs_64, R.raw.hs_65,
+                R.raw.hs_66, R.raw.hs_67, R.raw.hs_68, R.raw.hs_69, R.raw.hs_70, R.raw.hs_71,
+                R.raw.hs_72};
+
+        textView.setText(Utils.readFileFromRaw(this, R.raw.hs_01));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(Utils.readFileFromRaw(getApplicationContext(),
+                        resources[Utils.randomNum(NUMBER_OF_LISTS)]));
+            }
+        });
+
+
 
     }
 }
--- app/src/main/java/com/projs/liway/harvardsentences/Utils.java
+++ app/src/main/java/com/projs/liway/harvardsentences/Utils.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 /**
  * Created by Liway on 1/29/2018.
@@ -15,24 +16,24 @@ import java.io.InputStreamReader;
 
 public final class Utils {
 
-    public static final String LOG_TAG = "Utils";
+    private static final String LOG_TAG = "Utils";
     private Utils(){
     }
 
     /**
      * returns a String containing raw file contents
      */
-    public static String readFileFromRaw(Context context){
+    public static String readFileFromRaw(Context context, int resFile){
 
         String rawFileString = "";
         try{
-            InputStream is = context.getResources().openRawResource(R.raw.hs_01);
+            InputStream is = context.getResources().openRawResource(resFile);
 
             if(is != null){
                 InputStreamReader inputStreamReader = new InputStreamReader(is);
                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
                 StringBuilder stringBuilder = new StringBuilder();
-                String line = "";
+                String line;
                 while((line = bufferedReader.readLine()) != null){
                     stringBuilder.append(line + '\n' + '\n');
                 }
@@ -45,6 +46,11 @@ public final class Utils {
         }
 
         return rawFileString;
+    }
+
+    public static int randomNum(int n){
+        Random random = new Random();
+        return random.nextInt(n);
     }
 
 
